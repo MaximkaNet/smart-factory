fc54c02e730bd348bbaa1c6e8f0c18de88cc7f0e
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-15 19:01:40 +0100

    both file formatted

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/Machine.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/Machine.java
@@ -1,14 +1,12 @@
 package cz.cvut.fel.omo.smartfactory.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-public class Machine extends AbstractManufacturingEntity{
+public class Machine extends AbstractManufacturingEntity {
 
 }
