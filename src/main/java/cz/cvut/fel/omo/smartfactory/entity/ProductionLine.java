aeb360f2affdca56c001dbe5aaf39f5aeb1828d5
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-15 18:52:23 +0100

    one yo many relationship added to production line

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/ProductionLine.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/ProductionLine.java
@@ -3,8 +3,12 @@ package cz.cvut.fel.omo.smartfactory.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +17,8 @@ public class ProductionLine {
     @Id
     @GeneratedValue
     private Long id;
+
+    @OneToMany
+    @JoinColumn
+    private List<Person> people;
 }
