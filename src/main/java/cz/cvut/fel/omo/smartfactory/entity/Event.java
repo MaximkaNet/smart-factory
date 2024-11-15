7e618acef81c30bd2e09d7939da23c3b8a31b9f9
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-15 19:03:23 +0100

    Event added

---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/Event.java
@@ -0,0 +1,16 @@
+package cz.cvut.fel.omo.smartfactory.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
