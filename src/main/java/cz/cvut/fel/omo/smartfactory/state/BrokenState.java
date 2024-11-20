c76f266e249f899cd710192f938575333a342a9d
Maksym Zavada <zavadmak@fel.cvut.cz>
2024-11-20 14:00:03 +0100

    Add state

---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/state/BrokenState.java
@@ -0,0 +1,4 @@
+package cz.cvut.fel.omo.smartfactory.state;
+
+public class BrokenState implements ManufacturingEntityState {
+}
---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/state/ManufacturingEntityState.java
@@ -0,0 +1,4 @@
+package cz.cvut.fel.omo.smartfactory.state;
+
+public interface ManufacturingEntityState {
+}
