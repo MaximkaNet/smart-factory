8e9263b2ccd4de3ba6fba6fa318a07f1cd29b7f6
Maksym Zavada <m.zavada2005@ukr.net>
2024-11-20 14:00:03 +0100

    Add state

---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/state/ReadyState.java
@@ -0,0 +1,11 @@
+package cz.cvut.fel.omo.smartfactory.state;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReadyState implements ManufacturingEntityState {
+    private Long id;
+
+}
---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/state/RunningState.java
@@ -0,0 +1,4 @@
+package cz.cvut.fel.omo.smartfactory.state;
+
+public class RunningState implements ManufacturingEntityState {
+}
