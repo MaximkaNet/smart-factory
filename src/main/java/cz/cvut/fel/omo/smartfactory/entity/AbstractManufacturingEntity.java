0c58428bce24e6f982c17d0bdb39c44f42c16ca2
Maksym Zavada <m.zavada2005@ukr.net>
2024-11-20 13:59:13 +0100

    Remove rest & fix AbstractManufacturingEntity

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/AbstractManufacturingEntity.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/AbstractManufacturingEntity.java
@@ -2,11 +2,6 @@ package cz.cvut.fel.omo.smartfactory.entity;
 
 import cz.cvut.fel.omo.smartfactory.state.ManufacturingEntityState;
 import cz.cvut.fel.omo.smartfactory.state.ReadyState;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
 import lombok.Getter;
 import lombok.Setter;
 
