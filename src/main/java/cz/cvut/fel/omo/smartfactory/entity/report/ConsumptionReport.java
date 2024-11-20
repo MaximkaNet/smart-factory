5694c80bf083d98685772e8d9d3b268ea477b0f3
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-20 14:58:35 +0100

    reports added

---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/ConsumptionReport.java
@@ -0,0 +1,6 @@
+package cz.cvut.fel.omo.smartfactory.entity.report;
+
+import cz.cvut.fel.omo.smartfactory.entity.person.Person;
+
+public class ConsumptionReport extends Report{
+}
---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/EventReport.java
@@ -0,0 +1,4 @@
+package cz.cvut.fel.omo.smartfactory.entity.report;
+
+public class EventReport extends Report{
+}
