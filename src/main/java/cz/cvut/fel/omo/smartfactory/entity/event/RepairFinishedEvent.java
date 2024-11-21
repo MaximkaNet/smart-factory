6a562321fb258bfe83a4695d877e799bb9e3cc06
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-21 20:02:55 +0100

    check added

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/event/RepairFinishedEvent.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/event/RepairFinishedEvent.java
@@ -1,4 +1,16 @@
 package cz.cvut.fel.omo.smartfactory.entity.event;
 
+import cz.cvut.fel.omo.smartfactory.entity.person.Person;
+
 public class RepairFinishedEvent extends Event{
+    public RepairFinishedEvent(Integer priority) {
+        super(priority);
+    }
+
+    @Override
+    public boolean check(Person person) {
+        System.out.println("Person: " + person + " has checked repair finished on: ...");
+        setChecked(true);
+        return true;
+    }
 }
