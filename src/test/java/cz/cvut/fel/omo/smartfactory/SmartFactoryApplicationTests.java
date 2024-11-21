b41bf48a2203f2ca02ecfee587b6dc2c183c9f5b
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-21 20:03:14 +0100

    tests remade from spring boot to regular java

--- src/test/java/cz/cvut/fel/omo/smartfactory/SmartFactoryApplicationTests.java
+++ src/test/java/cz/cvut/fel/omo/smartfactory/SmartFactoryApplicationTests.java
@@ -1,9 +1,7 @@
 package cz.cvut.fel.omo.smartfactory;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SmartFactoryApplicationTests {
 
     @Test
