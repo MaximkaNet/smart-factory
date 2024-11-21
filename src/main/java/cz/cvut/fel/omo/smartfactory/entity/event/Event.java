f21ef815cd589eb5028847e1164533b9d84cc22a
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-21 20:01:43 +0100

    constructor implemented, plus check made abstract

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/event/Event.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/event/Event.java
@@ -1,17 +1,24 @@
 package cz.cvut.fel.omo.smartfactory.entity.event;
 
+import cz.cvut.fel.omo.smartfactory.entity.AbstractManufacturingEntity;
 import cz.cvut.fel.omo.smartfactory.entity.person.Person;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.ZonedDateTime;
+
 @Getter
 @Setter
 abstract public class Event {
-    private boolean isChecked;
-    private Integer priority;
+    protected Integer priority;
+    protected ZonedDateTime generatedAt;
+    protected AbstractManufacturingEntity abstractManufacturingEntity;
+    protected boolean isChecked = false;
 
-    public boolean check(Person person){
-        // TODO
-        return false;
+    public Event(Integer priority) {
+        this.generatedAt = ZonedDateTime.now();
+        this.priority = priority;
     }
+
+    abstract public boolean check(Person person);
 }
