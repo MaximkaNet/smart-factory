15b9ae6a5f397f13ae07bcb901e083c5d3f45a20
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-21 20:04:05 +0100

    repairman pool implemented

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/RepairmanPool.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/RepairmanPool.java
@@ -1,4 +1,46 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
+import cz.cvut.fel.omo.smartfactory.entity.event.OutageEvent;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.PriorityQueue;
+
+@Getter
+@Setter
 public class RepairmanPool {
+    PriorityQueue<OutageEvent> outageEventsQueue;
+    private List<Repairman> repairmenList;
+
+    public RepairmanPool(List<Repairman> repairmenList) {
+        this.repairmenList = repairmenList;
+        this.outageEventsQueue = new PriorityQueue<>();
+    }
+
+    public void addOutageEvent(OutageEvent outageEvent) {
+        if (outageEventsQueue.contains(outageEvent)) {
+            return;
+        }
+        outageEventsQueue.add(outageEvent);
+    }
+
+    public synchronized Optional<OutageEvent> getMostUrgentEvent() {
+        if (outageEventsQueue.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(outageEventsQueue.poll());
+    }
+
+    public void executeRepairs() {
+        while (!outageEventsQueue.isEmpty()) {
+            repairmenList.parallelStream()
+                    .filter(Repairman::isAvailable)
+                    .forEach(repairman -> {
+                        Optional<OutageEvent> nextEvent = getMostUrgentEvent();
+                        nextEvent.ifPresent(outageEvent -> outageEvent.check(repairman));
+                    });
+        }
+    }
 }
