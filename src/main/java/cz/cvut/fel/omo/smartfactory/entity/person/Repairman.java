66868a7fe20cd9ede347ebea702ffcd72062f6e5
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-21 20:03:49 +0100

    repairman logic implemented
    constructor, toString and the repair is just simulated now, will have to be implemented properly

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Repairman.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Repairman.java
@@ -1,4 +1,31 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
-public class Repairman extends Person{
+import cz.cvut.fel.omo.smartfactory.entity.AbstractManufacturingEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Repairman extends Person {
+    private boolean isAvailable;
+
+    public Repairman(String firstName, String lastName, String email) {
+        super(firstName, lastName, email);
+    }
+
+    public void repair(AbstractManufacturingEntity abstractManufacturingEntity) {
+        // TODO: implement repairing just a simulation right now
+        // TODO: create RepairStartedEvent
+        try {
+            Thread.sleep(1000);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        // TODO: create RepairFinishedEvent
+    }
+
+    @Override
+    public String toString() {
+        return "Repairman{" + super.toString() + ", isAvailable=" + isAvailable + '}';
+    }
 }
