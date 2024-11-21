92453c04f21bdc92ada99fb9baf5981f66ba708f
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-21 20:01:00 +0100

    constructor added to Person

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Inspector.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Inspector.java
@@ -1,4 +1,9 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
+import cz.cvut.fel.omo.smartfactory.entity.person.personState.PersonState;
+
 public class Inspector extends Person{
+    public Inspector(String firstName, String lastName, String email) {
+        super(firstName, lastName, email);
+    }
 }
--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Person.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Person.java
@@ -1,5 +1,6 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
+import cz.cvut.fel.omo.smartfactory.entity.person.personState.IdleState;
 import cz.cvut.fel.omo.smartfactory.entity.person.personState.PersonState;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,4 +12,16 @@ abstract public class Person {
     private String lastName;
     private String email;
     private PersonState state;
+
+    public Person(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.state = new IdleState(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", state=" + state + '}';
+    }
 }
