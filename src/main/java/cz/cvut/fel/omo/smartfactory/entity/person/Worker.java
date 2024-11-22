96dda319e5c2438ebc584ba8657021330b09bd21
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-22 17:58:46 +0100

    unnecessary imports removed

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Worker.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Worker.java
@@ -1,5 +1,6 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
+import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
 import cz.cvut.fel.omo.smartfactory.entity.person.personState.PersonState;
 
 public class Worker extends Person{
