bcb9aa1e8cfd96c0d8050215f17694ecca753953
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-22 18:00:18 +0100

    default constructor added and toString added
    factory will have to be inserted into this constructor, this will be managed by the factory builder

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/FactoryConfigurationReport.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/FactoryConfigurationReport.java
@@ -1,4 +1,9 @@
 package cz.cvut.fel.omo.smartfactory.entity.report;
 
+import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
+
 public class FactoryConfigurationReport extends Report{
+    public FactoryConfigurationReport(Factory factory) {
+        super(factory);
+    }
 }
