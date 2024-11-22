bb25516830085d004ce00936a11b2f66d549e44a
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-22 17:59:02 +0100

    unnecessary imports removed

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/ConsumptionReport.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/ConsumptionReport.java
@@ -1,6 +1,10 @@
 package cz.cvut.fel.omo.smartfactory.entity.report;
 
+import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
 import cz.cvut.fel.omo.smartfactory.entity.person.Person;
 
 public class ConsumptionReport extends Report{
+    public ConsumptionReport(Factory factory) {
+        super(factory);
+    }
 }
