47f08e8a645558f2223820ae87325dd31bf175e3
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-22 18:00:18 +0100

    default constructor added and toString added
    factory will have to be inserted into this constructor, this will be managed by the factory builder

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/EventReport.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/EventReport.java
@@ -1,4 +1,9 @@
 package cz.cvut.fel.omo.smartfactory.entity.report;
 
+import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
+
 public class EventReport extends Report{
+    public EventReport(Factory factory) {
+        super(factory);
+    }
 }
--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/Report.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/Report.java
@@ -1,9 +1,20 @@
 package cz.cvut.fel.omo.smartfactory.entity.report;
 
+import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 abstract public class Report {
+    protected Factory factory;
+
+    public Report(Factory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + ", factory=" + factory + "]";
+    }
 }
