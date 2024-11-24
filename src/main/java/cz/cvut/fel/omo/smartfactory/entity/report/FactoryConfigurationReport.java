2918b37470c4a8361e8663c40780f608fb6f57ad
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-24 21:16:11 +0100

    constructor accepting from to now

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/FactoryConfigurationReport.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/FactoryConfigurationReport.java
@@ -2,8 +2,11 @@ package cz.cvut.fel.omo.smartfactory.entity.report;
 
 import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
 
+import java.time.ZonedDateTime;
+
 public class FactoryConfigurationReport extends Report{
-    public FactoryConfigurationReport(Factory factory) {
-        super(factory);
+
+    public FactoryConfigurationReport(ZonedDateTime from, ZonedDateTime to, Factory factory) {
+        super(from, to, factory);
     }
 }
