e51d3a2c9a1b66acf555aacaf7329dcc319cc7c3
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-25 15:00:55 +0100

    registering methods added to event facade

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/event/EventFacade.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/event/EventFacade.java
@@ -2,10 +2,14 @@ package cz.cvut.fel.omo.smartfactory.entity.event;
 
 import cz.cvut.fel.omo.smartfactory.entity.AbstractManufacturingEntity;
 import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
+import cz.cvut.fel.omo.smartfactory.entity.person.Repairman;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,28 +17,42 @@ public class EventFacade {
     private final List<OutageEvent> outageEvents = new ArrayList<>();
     private final List<RepairFinishedEvent> repairFinishedEvents = new ArrayList<>();
     private final List<RepairStartedEvent> repairStartedEvents = new ArrayList<>();
+    private final Map<Class<?>, List<Eventable>> eventTypeListenersMap = new HashMap<>();
     private Factory factory;
 
     public EventFacade(Factory factory) {
         this.factory = factory;
     }
 
+    public void registerAllForEventType(Map<Class<?>, List<Eventable>> eventTypeListenersMap) {
+        eventTypeListenersMap.forEach((eventType, newListeners) ->
+                this.eventTypeListenersMap.computeIfAbsent(eventType, k -> new ArrayList<>()).addAll(newListeners));
+    }
+
+    private List<Eventable> getListenersForEventType(Class<?> eventType) {
+        return eventTypeListenersMap.getOrDefault(eventType, Collections.emptyList());
+    }
+
     public void addOutageEvent(Integer priority, AbstractManufacturingEntity abstractManufacturingEntity) {
         OutageEvent event = new OutageEvent(priority, abstractManufacturingEntity);
         outageEvents.add(event);
         factory.getRepairmanPool().addOutageEvent(event);
+        getListenersForEventType(OutageEvent.class).forEach(listener -> listener.receiveEvent(event));
     }
 
-    public void addRepairFinishedEvent(OutageEvent outageEvent) {
-        RepairFinishedEvent repairFinishedEvent = new RepairFinishedEvent(outageEvent.getPriority(), outageEvent);
+    public void addRepairFinishedEvent(Repairman repairman, OutageEvent outageEvent) {
+        RepairFinishedEvent repairFinishedEvent = new RepairFinishedEvent(outageEvent.getPriority(), repairman, outageEvent);
         repairFinishedEvents.add(repairFinishedEvent);
+        getListenersForEventType(RepairFinishedEvent.class).forEach(listener -> listener.receiveEvent(repairFinishedEvent));
     }
 
-    public void addRepairStartedEvent(OutageEvent outageEvent) {
-        RepairStartedEvent repairStartedEvent = new RepairStartedEvent(outageEvent.getPriority(), outageEvent);
+    public void addRepairStartedEvent(Repairman repairman, OutageEvent outageEvent) {
+        RepairStartedEvent repairStartedEvent = new RepairStartedEvent(outageEvent.getPriority(), repairman, outageEvent);
         repairStartedEvents.add(repairStartedEvent);
+        getListenersForEventType(RepairStartedEvent.class).forEach(listener -> listener.receiveEvent(repairStartedEvent));
     }
 
+    // ----------------------------------------------------- GETTING EVENTS -----------------------------------------------------
     public List<Event> getSortedEvents() {
         return Stream.of(outageEvents, repairFinishedEvents, repairStartedEvents)
                 .flatMap(List::stream)
