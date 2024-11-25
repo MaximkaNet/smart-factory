ba9753a623f6b90dd7f879552bcca1d222d0cfb7
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-25 15:01:20 +0100

    AbstractManufacturingEntity changed into more abstract Eventable

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/EventReport.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/EventReport.java
@@ -2,6 +2,7 @@ package cz.cvut.fel.omo.smartfactory.entity.report;
 
 import cz.cvut.fel.omo.smartfactory.entity.AbstractManufacturingEntity;
 import cz.cvut.fel.omo.smartfactory.entity.event.Event;
+import cz.cvut.fel.omo.smartfactory.entity.event.Eventable;
 import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
 import cz.cvut.fel.omo.smartfactory.entity.person.Person;
 
@@ -14,7 +15,7 @@ import java.util.stream.Collectors;
 public class EventReport extends Report{
     List<Event> events;
     Map<Class<?>, List<Event>> eventTypeMap;
-    Map<AbstractManufacturingEntity, List<Event>> eventSourceMap;
+    Map<Eventable, List<Event>> eventSourceMap;
     Map<Optional<Person>, List<Event>> eventCheckerMap;
 
     public EventReport(ZonedDateTime from, ZonedDateTime to, Factory factory) {
@@ -26,7 +27,7 @@ public class EventReport extends Report{
                 .collect(Collectors.groupingBy(Event::getClass));
 
         this.eventSourceMap = events.stream()
-                .collect(Collectors.groupingBy(Event::getAbstractManufacturingEntity));
+                .collect(Collectors.groupingBy(Event::getSender));
 
         this.eventCheckerMap = events.stream()
                 .collect(Collectors.groupingBy(event -> Optional.ofNullable(event.getCheckedBy())));
