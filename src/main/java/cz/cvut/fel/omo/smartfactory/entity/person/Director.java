62039087c57be945f44984fa4a5cef6f5f7260d5
Maksym Zavada <m.zavada2005@ukr.net>
2024-11-29 18:35:01 +0100

    Refactor entities in person package & refactor RepairmanPool logic

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Director.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Director.java
@@ -1,10 +1,9 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
 import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
-import cz.cvut.fel.omo.smartfactory.entity.person.personState.PersonState;
 
-public class Director extends Person{
-    public Director(String firstName, String lastName, String email) {
-        super(firstName, lastName, email);
+public class Director extends Person {
+    public Director(Factory factory, String firstName, String lastName) {
+        super(factory, firstName, lastName);
     }
 }
--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Worker.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Worker.java
@@ -1,10 +1,9 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
 import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
-import cz.cvut.fel.omo.smartfactory.entity.person.personState.PersonState;
 
-public class Worker extends Person{
-    public Worker(String firstName, String lastName, String email) {
-        super(firstName, lastName, email);
+public class Worker extends Person {
+    public Worker(Factory factory, String firstName, String lastName) {
+        super(factory, firstName, lastName);
     }
 }
