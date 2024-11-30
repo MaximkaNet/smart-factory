c1775702612eb6b219cda4f00ac8965c551c78ac
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-30 14:33:40 +0100

    robots machines people lists were added into factory and the main tact loop was remade to not be asynchronous and have added restore,inspect methods however they are not implemented yet

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/factory/Factory.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/factory/Factory.java
@@ -1,13 +1,18 @@
 package cz.cvut.fel.omo.smartfactory.entity.factory;
 
+import cz.cvut.fel.omo.smartfactory.entity.Machine;
+import cz.cvut.fel.omo.smartfactory.entity.ProductionLine;
+import cz.cvut.fel.omo.smartfactory.entity.Robot;
 import cz.cvut.fel.omo.smartfactory.entity.event.EventFacade;
 import cz.cvut.fel.omo.smartfactory.entity.factory.factoryObserver.TactSubscriber;
 import cz.cvut.fel.omo.smartfactory.entity.person.Person;
 import cz.cvut.fel.omo.smartfactory.entity.person.RepairmanPool;
+import cz.cvut.fel.omo.smartfactory.entity.report.OutagesReport;
 import cz.cvut.fel.omo.smartfactory.entity.report.Report;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,45 +22,80 @@ public class Factory {
     private String name;
     private List<Report> reports;
     private Integer tactLengthMilliseconds = 500;
-    private Integer currentTact = 1;
+    private Integer currentTact = 0;
     private boolean isRunning = false;
     private List<Person> people;
+    private List<ProductionLine> productionLines;
+    private List<Robot> robots;
+    private List<Machine> machines;
     private RepairmanPool repairmanPool;
     private List<TactSubscriber> tactSubscribers = new ArrayList<>();
     private EventFacade eventFacade;
 
-    public Factory(String name) {
+    public Factory(String name, int tactLengthMilliseconds, List<Person> people, List<Machine> machines, List<Robot> robots, List<ProductionLine> productionLines) {
         this.name = name;
+        this.tactLengthMilliseconds = tactLengthMilliseconds;
+        this.productionLines = productionLines;
+        this.robots = robots;
+        this.machines = machines;
+        this.people = people;
         eventFacade = new EventFacade(this);
     }
 
-    public void startFactory() {
-        isRunning = true;
-        new Thread(() -> {
-            while (isRunning) {
-                System.out.println("Tact " + currentTact + " started");
-                runTact();
-                try {
-                    Thread.sleep(tactLengthMilliseconds);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                currentTact++;
-            }
-        }).start();
-    }
-
-    private void runTact() {
+    /**
+     * Main simulation method
+     */
+    public void simulate() {
+        currentTact++;
+        System.out.println("tact: " + currentTact);
+        // run everything that needs to be run on one tact
+        // send message about new tact for each tact subscriber
         tactSubscribers.forEach(subscriber -> subscriber.onNewTact(currentTact));
+        // execute next tact in repairmanPool
         repairmanPool.executeRepairs();
+        // production lines
+        for (ProductionLine productionLine : productionLines) {
+            productionLine.process();
+        }
+
+        // I have added sleeping here
+        // will be removed and is here to observe factory working
+        try {
+            Thread.sleep(tactLengthMilliseconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public boolean stopFactory() {
-        if (!isRunning) {
-            return false;
+    public void simulate(int ticks) {
+        for (int i = 0; i < ticks; i++) {
+            simulate();
         }
-        isRunning = false;
-        return true;
+    }
+
+    /**
+     * Create outage report in a time span
+     *
+     * @return stringify report
+     */
+    public OutagesReport createOutageReport(ZonedDateTime from, ZonedDateTime to) {
+        return new OutagesReport(from, to, this);
+    }
+
+    /**
+     * This method will be used in memento pattern
+     *
+     * @param date restore date
+     */
+    public void restore(String date) {
+        // TODO: restoring of the factory in memento pattern
+    }
+
+    /**
+     * Conduct an inspection of the factory
+     */
+    public void inspect() {
+        // TODO: inspector will iterate through factory and inspection will be performed
     }
 
     @Override
