3db29ee9326c80bc49d816b8b7082a91b100af59
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-30 20:54:54 +0100

    we won't be using FactoryEventType enumeration

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/event/FactoryEventType.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/event/FactoryEventType.java
@@ -1,5 +1,6 @@
 package cz.cvut.fel.omo.smartfactory.entity.event;
 
+// TODO: will be removed as we will wont be using enum
 public enum FactoryEventType {
     REPAIR_STARTED,
     REPAIR_COMPLETED,
