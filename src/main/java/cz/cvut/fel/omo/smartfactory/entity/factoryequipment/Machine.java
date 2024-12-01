84f9aa326e44db2932e2782661319457e44e6087
Maksym Zavada <zavadmak@fel.cvut.cz>
2024-12-01 17:21:31 +0100

    Update entities after abstract class rename

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/factoryequipment/Machine.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/factoryequipment/Machine.java
@@ -1,10 +1,18 @@
-package cz.cvut.fel.omo.smartfactory.entity;
+package cz.cvut.fel.omo.smartfactory.entity.factoryequipment;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Machine extends AbstractFactoryEquipment {
+public final class Machine extends AbstractFactoryEquipment {
 
+    public Machine(String id, float pricePerUsage) {
+        super(id, pricePerUsage);
+    }
+
+    @Override
+    public void process() {
+        // Do stuff
+    }
 }
