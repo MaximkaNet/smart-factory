d276eaf8bd98f29c5a3deea781793fe13e826836
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-02 17:51:24 +0100

    worker implements ProductionUnit

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Worker.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Worker.java
@@ -1,9 +1,45 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
-import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
+import cz.cvut.fel.omo.smartfactory.entity.Product;
+import cz.cvut.fel.omo.smartfactory.entity.ProductionUnit;
 
-public class Worker extends Person {
+public class Worker extends Person implements ProductionUnit {
     public Worker(String firstName, String lastName, String email) {
         super(firstName, lastName, email);
+    }
+
+    @Override
+    public String getId() {
+        return "";
+    }
+
+    @Override
+    public void process() {
+
+    }
+
+    @Override
+    public boolean accept(Product product) {
+        return false;
+    }
+
+    @Override
+    public Product pop() {
+        return null;
+    }
+
+    @Override
+    public void setNext(ProductionUnit unit) {
+
+    }
+
+    @Override
+    public Product processNext(Product product) {
+        return null;
+    }
+
+    @Override
+    public void update(long deltaTime) {
+
     }
 }
