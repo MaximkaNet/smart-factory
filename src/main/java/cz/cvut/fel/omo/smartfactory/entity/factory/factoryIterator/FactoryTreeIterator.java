aa05e5ca65d1274094e05ae24c02d9f293d00e3d
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-02 18:21:35 +0100

    factory tree iterator added
    for now the code is commented out as the needed implementations are not ready at this time

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/factory/factoryIterator/FactoryTreeIterator.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/factory/factoryIterator/FactoryTreeIterator.java
@@ -1,18 +1,51 @@
 package cz.cvut.fel.omo.smartfactory.entity.factory.factoryIterator;
 
+import cz.cvut.fel.omo.smartfactory.entity.ProductionLine;
 import cz.cvut.fel.omo.smartfactory.entity.ProductionUnit;
 import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
 
 import java.util.Iterator;
 
 public class FactoryTreeIterator implements Iterator<ProductionUnit> {
+    private Factory factory;
+    private ProductionLine productionLine;
+    private ProductionUnit current;
+
+    public FactoryTreeIterator(Factory factory, ProductionLine productionLine, ProductionUnit current) {
+        this.factory = factory;
+        this.productionLine = productionLine;
+        this.current = current;
+        // TODO: check if current can have the reference to desired entities
+//        this.factory = current.getFactory();
+//        this.productionLine = current.getProductionLine;
+    }
+
     @Override
     public boolean hasNext() {
         return false;
+
+//        if (current.getNext() != null) {
+//            return true;
+//        }
+//        int nextProductionUnitIndex = factory.getProductionLines().indexOf(productionLine) + 1;
+//        return nextProductionUnitIndex >= factory.getProductionLines().size();
     }
 
     @Override
     public ProductionUnit next() {
         return null;
+
+//        if (current.getNext() != null) {
+//            return current.getNext();
+//        }
+//        int nextProductionUnitIndex = factory.getProductionLines().indexOf(productionLine) + 1;
+//        if (nextProductionUnitIndex >= factory.getProductionLines().size()) {
+//            throw new NoSuchElementException("Iterator cannot return an element because is at end");
+//        }
+//        this.productionLine = factory.getProductionLines().get(nextProductionUnitIndex);
+//        return productionLine.getProductionUnitChain();
     }
 }
+
+// ----------------------------- NOTES -----------------------------
+// stromové hierarchie entit továrna ->* linka -> *(stroj|robot|člověk nebo výrobek)
