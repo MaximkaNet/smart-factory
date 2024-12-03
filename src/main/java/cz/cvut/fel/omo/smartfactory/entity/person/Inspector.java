961154050d7ce74aa04f3b8108662f38d1885b6d
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-03 18:34:05 +0100

    implemented visitor pattern for Director and Inspector

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Inspector.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Inspector.java
@@ -1,12 +1,36 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
-public class Inspector extends Person {
+import cz.cvut.fel.omo.smartfactory.entity.Product;
+import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.Machine;
+import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.Robot;
+
+public class Inspector extends Person implements FactoryVisitor {
     public Inspector(String firstName, String lastName, String email) {
         super(firstName, lastName, email);
     }
 
     @Override
     public void update(long deltaTime) {
+        // TODO: implement the update method
+    }
 
+    @Override
+    public void visit(Person person) {
+        System.out.println(this + " is visiting Person: " + person);
+    }
+
+    @Override
+    public void visit(Machine machine) {
+        System.out.println(this + " is visiting Machine: " + machine);
+    }
+
+    @Override
+    public void visit(Robot robot) {
+        System.out.println(this + " is visiting Robot: " + robot);
+    }
+
+    @Override
+    public void visit(Product product) {
+        System.out.println(this + " is visiting Product: " + product);
     }
 }
