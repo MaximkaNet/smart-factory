07f28a10cf7d797e0b625f251618cf9644f6db19
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-03 19:04:32 +0100

    getter added to get the current, also added the fields

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/factory/factoryIterator/FactoryPriorityIterator.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/factory/factoryIterator/FactoryPriorityIterator.java
@@ -1,17 +1,33 @@
 package cz.cvut.fel.omo.smartfactory.entity.factory.factoryIterator;
 
+import cz.cvut.fel.omo.smartfactory.entity.ProductionLine;
 import cz.cvut.fel.omo.smartfactory.entity.ProductionUnit;
+import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
+import lombok.Getter;
 
 import java.util.Iterator;
 
 public class FactoryPriorityIterator implements Iterator<ProductionUnit> {
+    private Factory factory;
+    private ProductionLine productionLine;
+    @Getter
+    private ProductionUnit current;
+
+    public FactoryPriorityIterator(Factory factory, ProductionLine productionLine, ProductionUnit current) {
+        this.factory = factory;
+        this.productionLine = productionLine;
+        this.current = current;
+    }
+
     @Override
     public boolean hasNext() {
         return false;
+        // TODO
     }
 
     @Override
     public ProductionUnit next() {
         return null;
+        // TODO
     }
 }
