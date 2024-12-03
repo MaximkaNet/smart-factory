400d1d43caf6a01f45f0804549f6df3c09b74f59
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-03 19:03:44 +0100

    director and inspector implements visitor pattern

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Director.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Director.java
@@ -1,36 +1,58 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
 import cz.cvut.fel.omo.smartfactory.entity.Product;
+import cz.cvut.fel.omo.smartfactory.entity.factory.factoryIterator.FactoryTreeIterator;
 import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.Machine;
 import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.Robot;
 
 public class Director extends Person implements FactoryVisitor {
+    private FactoryTreeIterator iterator;
+
     public Director(String firstName, String lastName, String email) {
         super(firstName, lastName, email);
     }
 
+    public void startVisit(FactoryTreeIterator iterator) {
+        if (state.isAvailable()) {
+            state.work();
+        } else {
+            throw new IllegalStateException("Cannot start visiting as " + this + " is not available");
+        }
+
+        this.iterator = iterator;
+    }
+
     @Override
     public void update(long deltaTime) {
-        // TODO: implement the update method
+        if (!state.isExecuting()) {
+            return;
+        }
+
+        // TODO: ProductionUnit has to implement the accept method for the visitor
+//        iterator.getCurrent().accept(this);
+
+        if (iterator.hasNext()) {
+            iterator.next();
+        }
     }
 
     @Override
     public void visit(Person person) {
-        System.out.println(this + " is visiting Person: " + person);
+        System.out.println(this + " is checking Person: " + person);
     }
 
     @Override
     public void visit(Machine machine) {
-        System.out.println(this + " is visiting Machine: " + machine);
+        System.out.println(this + " is checking Machine: " + machine);
     }
 
     @Override
     public void visit(Robot robot) {
-        System.out.println(this + " is visiting Robot: " + robot);
+        System.out.println(this + " is checking Robot: " + robot);
     }
 
     @Override
     public void visit(Product product) {
-        System.out.println(this + " is visiting Product: " + product);
+        System.out.println(this + " is checking Product: " + product);
     }
 }
--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Inspector.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Inspector.java
@@ -1,36 +1,58 @@
 package cz.cvut.fel.omo.smartfactory.entity.person;
 
 import cz.cvut.fel.omo.smartfactory.entity.Product;
+import cz.cvut.fel.omo.smartfactory.entity.factory.factoryIterator.FactoryPriorityIterator;
 import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.Machine;
 import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.Robot;
 
 public class Inspector extends Person implements FactoryVisitor {
+    private FactoryPriorityIterator iterator;
+
     public Inspector(String firstName, String lastName, String email) {
         super(firstName, lastName, email);
     }
 
+    public void startVisit(FactoryPriorityIterator iterator) {
+        if (state.isAvailable()) {
+            state.work();
+        } else {
+            throw new IllegalStateException("Cannot start visiting as " + this + " is not available");
+        }
+
+        this.iterator = iterator;
+    }
+
     @Override
     public void update(long deltaTime) {
-        // TODO: implement the update method
+        if (!state.isExecuting()) {
+            return;
+        }
+
+        // TODO: ProductionUnit has to implement the accept method for the visitor
+//        iterator.getCurrent().accept(this);
+
+        if (iterator.hasNext()) {
+            iterator.next();
+        }
     }
 
     @Override
     public void visit(Person person) {
-        System.out.println(this + " is visiting Person: " + person);
+        System.out.println(this + " is inspecting Person: " + person);
     }
 
     @Override
     public void visit(Machine machine) {
-        System.out.println(this + " is visiting Machine: " + machine);
+        System.out.println(this + " is inspecting Machine: " + machine);
     }
 
     @Override
     public void visit(Robot robot) {
-        System.out.println(this + " is visiting Robot: " + robot);
+        System.out.println(this + " is inspecting Robot: " + robot);
     }
 
     @Override
     public void visit(Product product) {
-        System.out.println(this + " is visiting Product: " + product);
+        System.out.println(this + " is inspecting Product: " + product);
     }
 }
