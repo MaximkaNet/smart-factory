cf59e4b156caa9ebf867b13e3b74fcc8ea79db50
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-04 15:47:35 +0100

    added discriminator to Person

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Person.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Person.java
@@ -2,8 +2,8 @@ package cz.cvut.fel.omo.smartfactory.entity.person;
 
 import cz.cvut.fel.omo.smartfactory.entity.event.FactoryEvent;
 import cz.cvut.fel.omo.smartfactory.entity.event.FactoryEventListener;
-import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
 import cz.cvut.fel.omo.smartfactory.entity.factory.Behavioral;
+import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
 import cz.cvut.fel.omo.smartfactory.entity.person.personState.PersonState;
 import cz.cvut.fel.omo.smartfactory.entity.person.personState.ReadyState;
 import lombok.Getter;
@@ -12,22 +12,22 @@ import lombok.Setter;
 @Getter
 @Setter
 abstract public class Person implements Behavioral, FactoryEventListener {
+    String discriminator;
     protected String firstName;
     protected String lastName;
-    protected String email;
     protected PersonState state;
     protected Factory factory;
 
-    public Person(String firstName, String lastName, String email) {
+    public Person(String discriminator, String firstName, String lastName) {
+        this.discriminator = discriminator;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
         this.state = new ReadyState(this);
     }
 
     @Override
     public String toString() {
-        return this.getClass().getSimpleName() + "{" + "firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", state=" + state + '}';
+        return this.getClass().getSimpleName() + "{" + "firstName=" + firstName + ", lastName=" + lastName + ", state=" + state + '}';
     }
 
     @Override
