5cd1e4306d5ae2370a208d65f214d28ff9846218
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-04 15:46:16 +0100

    FactoryTests updated to work correctly

--- src/test/java/cz/cvut/fel/omo/smartfactory/factory/FactoryTests.java
+++ src/test/java/cz/cvut/fel/omo/smartfactory/factory/FactoryTests.java
@@ -1,50 +1,59 @@
 package cz.cvut.fel.omo.smartfactory.factory;
 
+import cz.cvut.fel.omo.smartfactory.entity.event.OutageEvent;
 import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
+import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.Machine;
 import cz.cvut.fel.omo.smartfactory.entity.person.RepairmanPool;
+import cz.cvut.fel.omo.smartfactory.entity.report.EventReport;
+import cz.cvut.fel.omo.smartfactory.entity.report.OutagesReport;
 import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactoryTests {
     @Test
-    public void createFactoryUsingFactoryBuilderAndRunIt() {
-//        Repairman r1 = new Repairman("1st firstName", "1st lastName", "1st email");
-//        Repairman r2 = new Repairman("2nd firstName", "2nd lastName", "2nd email");
-//        Repairman r3 = new Repairman("3rd firstName", "3rd lastName", "3rd email");
-//        r3.getState().stopWorking(); // one repairman is not working
-//        Director director = new Director("Director", "testing", "email@director.som");
-//        List<Person> people = new ArrayList<>(Arrays.asList(r1, r2, r3));
-//
-//        FactoryBuilder builder = new FactoryBuilder();
-//        Factory factory = builder
-//                .setTickLength(500)
-//                .setPeople(people)
-//                .addEventableForEvent(OutageEvent.class, director)
-//                .build();
-//
-//        Machine m1 = new Machine("M1", 1.5f, 1);
-//        m1.setActualHealth(0);
-//        Machine m2 = new Machine("M2", 1.5f, 1);
-//        m2.setActualHealth(0);
-//        Machine m3 = new Machine("M2", 2.5f, 1);
-//        m3.setActualHealth(0);
-//        Machine m4 = new Machine("M2", 1.5f, 1);
-//        m4.setActualHealth(0);
-//        Machine m5 = new Machine("M2", 1.5f, 1);
-//        m5.setActualHealth(0);
-//        factory.getEventManager().notifyListeners(new OutageEvent(2, m1));
-//        factory.getEventManager().notifyListeners(new OutageEvent(3, m2));
-//        factory.getEventManager().notifyListeners(new OutageEvent(1, m3));
-//        factory.getEventManager().notifyListeners(new OutageEvent(0, m4));
-//        factory.getEventManager().notifyListeners(new OutageEvent(0, m5));
-//
-//        factory.simulate(15);
-//
-//        OutagesReport outagesReport = new OutagesReport(ZonedDateTime.now().minusMinutes(1), ZonedDateTime.now(), factory);
-//        System.out.println(outagesReport);
-//        EventReport eventReport = new EventReport(ZonedDateTime.now().minusMinutes(1), ZonedDateTime.now(), factory);
-//        System.out.println(eventReport);
+    public void createFactoryUsingFactoryBuilderAndRunIt() throws InterruptedException {
+        Factory factory = Factory.builder()
+                .setName("Factory 1")
+                .setTickLength(250)
+                .setRepairmanPool(
+                        RepairmanPool.builder()
+                                .addRepairman("Janko", "First", .2f)
+                                .addRepairman("Janko", "Second", .2f)
+                                .addRepairman("Janko", "Third", .2f)
+                                .build()
+                )
+                .addDirector("D", "Adam", "Boss")
+                .addInspector("I", "Laco", "Inspector")
+                .addMachine("M", "M1", 200f, 1)
+                .addMachine("M", "M2", 350f, 1)
+                .addMachine("M", "M3", 400f, 1)
+                .addMachine("M", "M4", 100f, 1)
+                .addMachine("M", "M5", 200f, 1)
+                .build();
+
+        // one repairman is not working
+        factory.getRepairmanPool().getRepairmenList().get(0).getState().stopWorking();
+
+        factory.getEventManager().notifyListeners(new OutageEvent(2, factory.getMachines().get(0)));
+        factory.getEventManager().notifyListeners(new OutageEvent(3, factory.getMachines().get(1)));
+        factory.getEventManager().notifyListeners(new OutageEvent(1, factory.getMachines().get(2)));
+        factory.getEventManager().notifyListeners(new OutageEvent(0, factory.getMachines().get(3)));
+        factory.getEventManager().notifyListeners(new OutageEvent(0, factory.getMachines().get(4)));
+
+        // setting actual health to 0
+        for (Machine machine : factory.getMachines()) {
+            machine.setActualHealth(0);
+        }
+
+        factory.simulateRealtime(15);
+
+        OutagesReport outagesReport = new OutagesReport(ZonedDateTime.now().minusMinutes(1), ZonedDateTime.now(), factory);
+        System.out.println(outagesReport);
+        EventReport eventReport = new EventReport(ZonedDateTime.now().minusMinutes(1), ZonedDateTime.now(), factory);
+        System.out.println(eventReport);
     }
 
     @Test
@@ -74,6 +83,6 @@ public class FactoryTests {
         factory.simulate(10);
 
         assertEquals("Factory", factory.getName());
-        assertEquals(200, factory.getTactLengthMilliseconds());
+        assertEquals(200, factory.getTickLengthMillis());
     }
 }
