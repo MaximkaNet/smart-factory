28a135a055a136b99e64bc9022ff6964eab38e0b
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-07 20:29:12 +0100

    tests for not trivial logic builder implemented and also imported mockito

---
+++ src/test/java/cz/cvut/fel/omo/smartfactory/factory/FactoryBuilderTest.java
@@ -0,0 +1,66 @@
+package cz.cvut.fel.omo.smartfactory.factory;
+
+import cz.cvut.fel.omo.smartfactory.entity.event.OutageEvent;
+import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
+import cz.cvut.fel.omo.smartfactory.entity.person.RepairmanPool;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class FactoryBuilderTest {
+
+    @Test
+    public void factoryBuilderSetsRepairmanPoolAsBehavioral() {
+        RepairmanPool mockRepairmanPool = mock(RepairmanPool.class);
+
+        Factory factory = Factory.builder()
+                .setRepairmanPool(
+                        mockRepairmanPool
+                )
+                .build();
+
+        factory.simulate(10);
+        verify(mockRepairmanPool, times(10)).update(anyLong());
+    }
+
+    @Test
+    public void FactoryBuilderSetsRepairmanToListenToOutageEvent() {
+        RepairmanPool mockRepairmanPool = mock(RepairmanPool.class);
+
+        Factory factory = Factory.builder()
+                .setRepairmanPool(
+                        mockRepairmanPool
+                )
+                .addMachine("M", "machine", 10, 10)
+                .build();
+
+        factory.getEventManager().notifyListeners(new OutageEvent(1, factory.getMachines().get(0), Instant.now()));
+        factory.getEventManager().notifyListeners(new OutageEvent(2, factory.getMachines().get(0), Instant.now()));
+        factory.simulate(5);
+        verify(mockRepairmanPool, times(2)).receiveEvent(any());
+    }
+
+    @Test
+    public void factoryBuilderSetsBehavioralsForAllProductionUnits() {
+        Factory factory = Factory.builder()
+                .setRepairmanPool(
+                        RepairmanPool.builder().build()
+                )
+                .addWorker("W", "first", "last", 10)
+                .addRobot("R", "name", 10, 10)
+                .addDirector("D", "first", "last")
+                .addInspector("I", "first", "last")
+                .addMachine("M", "name", 10, 10)
+                .build();
+
+        // 5 * ProductionUnits + RepairmanPool
+        assertEquals(6, factory.getBehavioralsList().size());
+    }
+}
