aead833c4b8517aa763759d701b2c58ec6e9fcb2
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-14 11:39:25 +0100

    System.out.println was replaced for logger

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Inspector.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/person/Inspector.java
@@ -4,8 +4,12 @@ import cz.cvut.fel.omo.smartfactory.entity.Product;
 import cz.cvut.fel.omo.smartfactory.entity.factory.factoryIterator.FactoryUsageIterator;
 import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.Machine;
 import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.Robot;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Inspector extends Person implements FactoryVisitor {
+    public static final Logger LOGGER = LogManager.getLogger("Inspector");
+
     private FactoryUsageIterator iterator;
 
     public Inspector(String firstName, String lastName, String email) {
@@ -33,28 +37,28 @@ public class Inspector extends Person implements FactoryVisitor {
         if (iterator.hasNext()) {
             iterator.next();
         } else {
-            System.out.println(this + " changed state to ready as the iteration is complete");
+            LOGGER.info("{} changed state to ready as the iteration is complete", this);
             state.ready();
         }
     }
 
     @Override
     public void visit(Person person) {
-        System.out.println(this + " is inspecting Person: " + person);
+        LOGGER.info("{} is inspecting Person: {}", this, person);
     }
 
     @Override
     public void visit(Machine machine) {
-        System.out.println(this + " is inspecting Machine: " + machine);
+        LOGGER.info("{} is inspecting Machine: {}", this, machine);
     }
 
     @Override
     public void visit(Robot robot) {
-        System.out.println(this + " is inspecting Robot: " + robot);
+        LOGGER.info("{} is inspecting Robot: {}", this, robot);
     }
 
     @Override
     public void visit(Product product) {
-        System.out.println(this + " is inspecting Product: " + product);
+        LOGGER.info("{} is inspecting Product: {}", this, product);
     }
 }
