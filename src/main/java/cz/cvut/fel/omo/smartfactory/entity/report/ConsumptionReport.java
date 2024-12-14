33b552411a0ca7a9541b7f0003727b53401be221
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-14 14:52:44 +0100

    ConsumptionReport implemented
    the from and to logic is not set and will have to be added later

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/ConsumptionReport.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/report/ConsumptionReport.java
@@ -1,12 +1,65 @@
 package cz.cvut.fel.omo.smartfactory.entity.report;
 
 import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
+import cz.cvut.fel.omo.smartfactory.entity.factoryequipment.AbstractFactoryEquipment;
+
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class ConsumptionReport extends Report{
+public class ConsumptionReport extends Report {
 
+    private List<AbstractFactoryEquipment> abstractFactoryEquipments = new ArrayList<>();
+
+    /**
+     * Report fields
+     */
+    private Map<AbstractFactoryEquipment, String> consumptionPerEquipment = new HashMap<>();
+    private String consumptionForFactory;
+
+    // TODO: the ZonedDateTime from and to are not considered in the calculation, should calculate consumption only in predefined interval
     public ConsumptionReport(ZonedDateTime from, ZonedDateTime to, Factory factory) {
         super(factory, from, to);
+
+        // loading all entities into a list of AbstractFactoryEquipment
+        abstractFactoryEquipments.addAll(factory.getMachines());
+        abstractFactoryEquipments.addAll(factory.getRobots());
+
+        calcFactoryConsumptionStats(factory);
+
+        calcConsumptionPerEquipment();
+    }
+
+    private void calcConsumptionPerEquipment() {
+        abstractFactoryEquipments.forEach(equipment -> consumptionPerEquipment.put(equipment,
+                equipment.getMaterialConsumer().getConsumed()
+                        + " " +
+                        equipment.getElectricityConsumer().getConsumed()
+                        + " " +
+                        equipment.getOilConsumer().getConsumed()
+        ));
+    }
+
+    private void calcFactoryConsumptionStats(Factory factory) {
+        Float electricityConsumption = abstractFactoryEquipments.stream()
+                .map(equipment -> equipment.getElectricityConsumer().getConsumed())
+                .reduce(0f, Float::sum);
+
+        Integer materialConsumption = abstractFactoryEquipments.stream()
+                .map(equipment -> equipment.getMaterialConsumer().getConsumed())
+                .reduce(0, Integer::sum);
+
+        Float oilConsumption = abstractFactoryEquipments.stream()
+                .map(equipment -> equipment.getOilConsumer().getConsumed())
+                .reduce(0f, Float::sum);
+
+        consumptionForFactory = "Factory " + factory + " consumed{ "
+                + "electricityConsumption: " + electricityConsumption
+                + " materialConsumption: " + materialConsumption
+                + " oilConsumption: " + oilConsumption
+                + "}";
     }
 
     @Override
