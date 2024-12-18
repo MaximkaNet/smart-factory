573d9ae0d5dc78a10f2ad90410983b0b86f82b54
Maksym Zavada <m.zavada2005@ukr.net>
2024-12-18 23:02:17 +0100

    Refactor factory equipment

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/equipment/AbstractEquipment.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/equipment/AbstractEquipment.java
@@ -1,8 +1,8 @@
-package cz.cvut.fel.omo.smartfactory.entity.factoryequipment;
+package cz.cvut.fel.omo.smartfactory.entity.equipment;
 
-import cz.cvut.fel.omo.smartfactory.entity.Material;
+import cz.cvut.fel.omo.smartfactory.entity.equipment.state.factory.EquipmentStateFactory;
 import cz.cvut.fel.omo.smartfactory.entity.productionunit.AbstractProductionUnit;
-import cz.cvut.fel.omo.smartfactory.entity.storage.Storage;
+import cz.cvut.fel.omo.smartfactory.entity.productionunit.state.ReadyState;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +11,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-public abstract class AbstractFactoryEquipment extends AbstractProductionUnit {
+public abstract class AbstractEquipment extends AbstractProductionUnit {
 
     /**
      * Maximum equipment health
@@ -24,66 +24,20 @@ public abstract class AbstractFactoryEquipment extends AbstractProductionUnit {
     private float actualHealth = 0;
 
     /**
-     * Material storage
-     */
-    private Storage<Material> materialStorage = null;
-
-
-//    /**
-//     * Price per usage
-//     */
-//    protected final float pricePerUsage;
-//
-//    /**
-//     * Electricity consumer
-//     */
-//    protected ElectricityConsumer electricityConsumer;
-//
-//    /**
-//     * Oil consumer
-//     */
-//    protected OilConsumer oilConsumer;
-//
-//    /**
-//     * Material consumer
-//     */
-//    protected MaterialConsumer materialConsumer;
-//
-//    /**
-//     * Number of usage (or just processed products)
-//     */
-//    protected float usageTime = 0.0f;
-
-    /**
      * Create factory equipment
      *
      * @param name   The equipment name
      * @param health The maximum health
      */
-    public AbstractFactoryEquipment(String name, float health) {
+    public AbstractEquipment(String name, float health) {
         super(name);
         this.maximumHealth = health;
+
+        // Set initial state
+        this.setState(new ReadyState(this, new EquipmentStateFactory()));
     }
 
     public boolean isBroken() {
         return false;
     }
-
-//    @Override
-//    public Product processNext(Product product, long dt) {
-//        if (next == null) {
-//            return product;
-//        }
-//        state.accept(product);
-//        state.process(dt);
-//        return processNext(state.pop(), dt);
-//    }
-
-//    /**
-//     * Generate outage event using event manager from factory
-//     */
-//    public void generateOutageEvent() {
-//        FactoryEvent event = new OutageEvent(123, this, factory.now());
-//        factory.getEventManager().notifyListeners(event);
-//    }
 }
