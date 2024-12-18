7169c70c616cbf7a75cf2798ff396dd567d5c883
Maksym Zavada <m.zavada2005@ukr.net>
2024-12-18 23:08:06 +0100

    Refactor Product

--- src/main/java/cz/cvut/fel/omo/smartfactory/entity/Product.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/entity/Product.java
@@ -12,20 +12,48 @@ public class Product {
     private String name;
 
     /**
-     * The list of ProductionUnit's
-     * <p>
-     * For example: RMRMRRMMWRWRW where R any robot, M any machine, W any worker
+     * Count of products
      */
-    private String sequence = "";
+    private long count;
 
+    /**
+     * Create single product
+     *
+     * @param name The product name
+     */
     public Product(String name) {
-        this.name = name;
+        this(name, 1);
     }
 
     /**
-     * Create a template of product
+     * Create stack of products
+     *
+     * @param name  The product name
+     * @param count The product count
      */
-    public Product createTemplate() {
-        return new Product(name);
+    public Product(String name, long count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    /**
+     * Consume the product
+     */
+    public void consume(Product product) {
+        if (product.getName().equals(name)) {
+            this.count += product.getCount();
+        } else {
+            throw new RuntimeException("The products are different");
+        }
+    }
+
+    /**
+     * Get specified number of products from this product
+     *
+     * @param count Number of product
+     */
+    public Product pop(long count) {
+        // return new instance
+        return null;
     }
 }
