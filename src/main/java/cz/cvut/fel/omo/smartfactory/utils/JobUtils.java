a8f2b2db4575cb7e3dfddca4e423f18346f85dca
Maksym Zavada <zavadmak@fel.cvut.cz>
2024-12-20 14:23:50 +0100

    Add JobUtils

---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/utils/JobUtils.java
@@ -0,0 +1,14 @@
+package cz.cvut.fel.omo.smartfactory.utils;
+
+public class JobUtils {
+    public static final float MAX_DURATION = 100.0f;
+    public static final float START_POINT = 0.0f;
+
+    /**
+     * Calculate step duration
+     */
+    public static float stepDuration(int steps) {
+        return 100.0f / steps;
+    }
+
+}
