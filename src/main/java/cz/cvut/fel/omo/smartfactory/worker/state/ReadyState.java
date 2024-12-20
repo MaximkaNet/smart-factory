fb232cdb38f9e4704f6a3db2c43bca5f950c2ff9
Maksym Zavada <zavadmak@fel.cvut.cz>
2024-12-20 14:18:44 +0100

    Refactor Worker and add states

---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/worker/state/ReadyState.java
@@ -0,0 +1,47 @@
+package cz.cvut.fel.omo.smartfactory.worker.state;
+
+import cz.cvut.fel.omo.smartfactory.Product;
+import cz.cvut.fel.omo.smartfactory.productionunit.AbstractProductionUnit;
+import cz.cvut.fel.omo.smartfactory.productionunit.AbstractProductionUnitState;
+
+/**
+ * Worker ready state
+ */
+public final class ReadyState extends AbstractProductionUnitState {
+    /**
+     * Create production unit state
+     *
+     * @param context The context
+     */
+    public ReadyState(AbstractProductionUnit context) {
+        super(context);
+    }
+
+    @Override
+    public void process(long dt) {
+    }
+
+    @Override
+    public boolean accept(Product product) {
+        if (context.accept(product)) {
+            context.setState(new ProcessingState(context));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Product pop() {
+        return null;
+    }
+
+    @Override
+    public Product peek() {
+        return null;
+    }
+
+    @Override
+    public boolean repair(float power) {
+        return true; // Unit already repaired
+    }
+}
