22847dd4a7ae3346967c7779f4cfd6f2172e989c
Maksym Zavada <m.zavada2005@ukr.net>
2024-12-23 23:14:00 +0100

    Update WorkerTest, remove ProductionLine state, refactor Product

--- src/main/java/cz/cvut/fel/omo/smartfactory/Product.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/Product.java
@@ -3,18 +3,16 @@ package cz.cvut.fel.omo.smartfactory;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * The product
+ */
 @Getter
 @Setter
-public class Product implements Stackable<Product> {
+public class Product {
     /**
      * The unique product name
      */
-    private String name;
-
-    /**
-     * Count of products
-     */
-    private long count;
+    private final String name;
 
     /**
      * Create single product
@@ -22,44 +20,6 @@ public class Product implements Stackable<Product> {
      * @param name The product name
      */
     public Product(String name) {
-        this(name, 1);
-    }
-
-    /**
-     * Create stack of products
-     *
-     * @param name  The product name
-     * @param count The product count
-     */
-    public Product(String name, long count) {
         this.name = name;
-        this.count = count;
-    }
-
-    /**
-     * Consume the product
-     */
-    @Override
-    public void consume(Product product) {
-        if (product.getName().equals(name)) {
-            this.count += product.getCount();
-            product.count = 0;
-        } else {
-            throw new RuntimeException("The products are different");
-        }
-    }
-
-    /**
-     * Get specified number of products from this product
-     *
-     * @param count Number of product
-     */
-    @Override
-    public Product pop(long count) {
-        if (this.count <= count) {
-            return this;
-        }
-        this.count -= Math.max(0, count);
-        return new Product(name, count);
     }
 }
