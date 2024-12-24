183fcbdcc7e75c7fc5e9be444b52e4988710493e
Maksym Zavada <zavadmak@fel.cvut.cz>
2024-12-24 14:40:05 +0100

    Remove timer dependency & create event bus manager

---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/event/EventBusManager.java
@@ -0,0 +1,23 @@
+package cz.cvut.fel.omo.smartfactory.event;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Event bus manager
+ */
+public class EventBusManager {
+    /**
+     * Buses
+     */
+    private static final Map<String, EventBus> buses = new HashMap<>();
+
+    /**
+     * Get event bus by name
+     *
+     * @param name The event bus name
+     */
+    public static EventBus getEventBus(String name) {
+        return buses.computeIfAbsent(name, k -> new EventBus());
+    }
+}
