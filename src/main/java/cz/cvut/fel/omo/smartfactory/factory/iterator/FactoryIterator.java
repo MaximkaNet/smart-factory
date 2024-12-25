fe553547f9cd331a4397686b57586789a3c2b6aa
Maksym Zavada <m.zavada2005@ukr.net>
2024-12-25 16:41:03 +0100

    Move iterators and add interface

---
+++ src/main/java/cz/cvut/fel/omo/smartfactory/factory/iterator/FactoryIterator.java
@@ -0,0 +1,11 @@
+package cz.cvut.fel.omo.smartfactory.factory.iterator;
+
+import cz.cvut.fel.omo.smartfactory.productionunit.AbstractProductionUnit;
+
+import java.util.Iterator;
+
+/**
+ * Factory iterator
+ */
+public interface FactoryIterator extends Iterator<AbstractProductionUnit> {
+}
