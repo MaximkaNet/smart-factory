a4be2db82808c06c41eff557760d7ef1dfdac744
Maksym Zavada <zavadmak@fel.cvut.cz>
2024-12-29 23:09:53 +0100

    Add comments and update package

--- src/main/java/cz/cvut/fel/omo/smartfactory/factory/TickObserver.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/factory/TickObserver.java
@@ -1,5 +1,14 @@
-package cz.cvut.fel.omo.smartfactory.entity.factory;
+package cz.cvut.fel.omo.smartfactory.factory;
 
+/**
+ * Tick observer
+ */
 public interface TickObserver {
+
+    /**
+     * Update function
+     *
+     * @param deltaTime The delta time
+     */
     void update(long deltaTime);
 }
