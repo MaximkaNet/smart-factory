98628bc4272b4ed10991fbd9a66609d22a11f736
Maksym Zavada <m.zavada2005@ukr.net>
2024-12-29 23:11:06 +0100

    Update factory test

--- src/test/java/cz/cvut/fel/omo/smartfactory/factory/FactoryTest.java
+++ src/test/java/cz/cvut/fel/omo/smartfactory/factory/FactoryTest.java
@@ -1,11 +1,34 @@
 package cz.cvut.fel.omo.smartfactory.factory;
 
+import cz.cvut.fel.omo.smartfactory.productionunit.ProductionUnitManager;
+import cz.cvut.fel.omo.smartfactory.repair.RepairmenPool;
+import cz.cvut.fel.omo.smartfactory.utils.JobUtils;
 import org.junit.jupiter.api.Test;
 
-public class FactoryTests {
+import java.util.List;
 
+public class FactoryTest {
     @Test
     void createFactoryAndSimulate() {
+        Factory factory = new Factory("Smart factory");
 
+        ProductionUnitManager unitManager = factory.getProductionUnitManager();
+
+        unitManager.createWorker("Worker", JobUtils.stepDuration(1));
+        unitManager.createWorker("Worker", JobUtils.stepDuration(1));
+        unitManager.createRobot("Robot", 100);
+        unitManager.createMachine("Machine", 100);
+
+        // Create repairmen
+        RepairmenPool pool = factory.getRepairmanPool();
+        pool.createRepairman("Repairman", 10.0f);
+        pool.createRepairman("Repairman", 10.0f);
+        pool.createRepairman("Repairman", 10.0f);
+        pool.createRepairman("Repairman", 10.0f);
+
+        factory.addOrder("Tesla model S", 10, List.of("W", "W", "R", "M"));
+
+        // Simulate
+        factory.simulate();
     }
 }
