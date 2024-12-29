57fb0b4721ae64a8f0359c8f32d88e1bb8fd774a
Maksym Zavada <m.zavada2005@ukr.net>
2024-12-29 23:29:32 +0100

    Refactor consumption report

--- src/main/java/cz/cvut/fel/omo/smartfactory/report/ConsumptionReport.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/report/ConsumptionReport.java
@@ -1,67 +1,50 @@
-package cz.cvut.fel.omo.smartfactory.entity.report;
+package cz.cvut.fel.omo.smartfactory.report;
 
-import cz.cvut.fel.omo.smartfactory.entity.equipment.AbstractEquipment;
-import cz.cvut.fel.omo.smartfactory.entity.factory.Factory;
+/**
+ * Consumption report
+ */
+public class ConsumptionReport implements Report {
 
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+    private final float oilConsumed;
+    private final float oilTotalPrice;
 
-public class ConsumptionReport extends Report {
+    private final float electricityConsumed;
+    private final float electricityTotalPrice;
 
-    private List<AbstractEquipment> abstractFactoryEquipments = new ArrayList<>();
+    private final long materialConsumed;
+    private final float materialTotalPrice;
 
     /**
-     * Report fields
+     * Consumption report
+     *
+     * @param oilConsumed
+     * @param oilTotalPrice
+     * @param electricityConsumed
+     * @param electricityTotalPrice
+     * @param materialConsumed
+     * @param materialTotalPrice
      */
-    private Map<AbstractEquipment, String> consumptionPerEquipment = new HashMap<>();
-    private String consumptionForFactory;
-
-    // TODO: the ZonedDateTime from and to are not considered in the calculation, should calculate consumption only in predefined interval
-    public ConsumptionReport(ZonedDateTime from, ZonedDateTime to, Factory factory) {
-        super(factory, from, to);
-
-        // loading all entities into a list of AbstractFactoryEquipment
-        abstractFactoryEquipments.addAll(factory.getMachines());
-        abstractFactoryEquipments.addAll(factory.getRobots());
-
-        calcFactoryConsumptionStats(factory);
-
-        calcConsumptionPerEquipment();
+    public ConsumptionReport(float oilConsumed, float oilTotalPrice, float electricityConsumed, float electricityTotalPrice, long materialConsumed, float materialTotalPrice) {
+        this.oilConsumed = oilConsumed;
+        this.oilTotalPrice = oilTotalPrice;
+        this.electricityConsumed = electricityConsumed;
+        this.electricityTotalPrice = electricityTotalPrice;
+        this.materialConsumed = materialConsumed;
+        this.materialTotalPrice = materialTotalPrice;
     }
 
-    private void calcConsumptionPerEquipment() {
-        abstractFactoryEquipments.forEach(equipment -> consumptionPerEquipment.put(equipment,
-                equipment.getMaterialConsumer().getConsumed()
-                        + " " +
-                        equipment.getElectricityConsumer().getConsumed()
-                        + " " +
-                        equipment.getOilConsumer().getConsumed()
-        ));
+    @Override
+    public String toString() {
+        return "Total consumption report: " +
+                System.lineSeparator() +
+                "- Material: " + materialConsumed + " pieces" + "/" + materialTotalPrice + "$" +
+                System.lineSeparator() +
+                "- Oil: " + oilConsumed + " l" + "/" + oilTotalPrice + "$" +
+                System.lineSeparator() +
+                "- Electricity: " + electricityConsumed + " kw" + "/" + electricityTotalPrice + "$"
+                + System.lineSeparator();
     }
-
-    private void calcFactoryConsumptionStats(Factory factory) {
-        Float electricityConsumption = abstractFactoryEquipments.stream()
-                .map(equipment -> equipment.getElectricityConsumer().getConsumed())
-                .reduce(0f, Float::sum);
-
-        Integer materialConsumption = abstractFactoryEquipments.stream()
-                .map(equipment -> equipment.getMaterialConsumer().getConsumed())
-                .reduce(0, Integer::sum);
-
-        Float oilConsumption = abstractFactoryEquipments.stream()
-                .map(equipment -> equipment.getOilConsumer().getConsumed())
-                .reduce(0f, Float::sum);
-
-        consumptionForFactory = "Factory " + factory + " consumed{ "
-                + "electricityConsumption: " + electricityConsumption
-                + " materialConsumption: " + materialConsumption
-                + " oilConsumption: " + oilConsumption
-                + "}";
-    }
-
+    
     @Override
     public String exportJson() {
         return "";
