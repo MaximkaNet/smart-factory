4383b8b3d9623a3147c46f3968cd7c899c874861
Maksym Zavada <zavadmak@fel.cvut.cz>
2024-12-29 23:17:28 +0100

    Refactor Snapshot

--- src/main/java/cz/cvut/fel/omo/smartfactory/memento/Snapshot.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/memento/Snapshot.java
@@ -1,8 +1,27 @@
 package cz.cvut.fel.omo.smartfactory.memento;
 
+import java.time.Instant;
+
 /**
  * The object snapshot
  */
-public class Snapshot {
+public abstract class Snapshot {
+    /**
+     * Created time
+     */
+    Instant createdAt;
 
+    /**
+     * Export snapshot to JSON
+     *
+     * @param pretty Pretty flag
+     */
+    public abstract String toJson(boolean pretty);
+
+    /**
+     * Export snapshot to JSON
+     */
+    public String toJson() {
+        return toJson(false);
+    }
 }
