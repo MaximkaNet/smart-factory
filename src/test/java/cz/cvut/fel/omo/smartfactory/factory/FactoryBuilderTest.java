5e6ac7838d377c301e77b706285e69b52cfe4a21
Jakub Janak <janakja5@fel.cvut.cz>
2025-01-01 20:20:24 +0100

    more advanced tests added for builder

--- src/test/java/cz/cvut/fel/omo/smartfactory/factory/FactoryBuilderTest.java
+++ src/test/java/cz/cvut/fel/omo/smartfactory/factory/FactoryBuilderTest.java
@@ -1,16 +1,19 @@
 package cz.cvut.fel.omo.smartfactory.factory;
 
+import cz.cvut.fel.omo.smartfactory.event.EventBusManager;
+import cz.cvut.fel.omo.smartfactory.event.EventType;
 import cz.cvut.fel.omo.smartfactory.productionunit.ProductionUnitManager;
 import cz.cvut.fel.omo.smartfactory.repair.RepairmenPool;
 import cz.cvut.fel.omo.smartfactory.utils.JobUtils;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactoryBuilderTest {
 
     @Test
-    void buildComplexFactory() {
+    void builderSetsFieldsCorrectly() {
 
         RepairmenPool pool = new RepairmenPool();
 
@@ -23,12 +26,15 @@ public class FactoryBuilderTest {
 
         productionUnitManager.createWorker("Worker", JobUtils.stepDuration(1));
         productionUnitManager.createWorker("Worker", JobUtils.stepDuration(1));
-        productionUnitManager.createWorker("Worker", JobUtils.stepDuration(1));
 
         productionUnitManager.createRobot("Robot", 100);
         productionUnitManager.createRobot("Robot", 100);
         productionUnitManager.createRobot("Robot", 100);
         productionUnitManager.createRobot("Robot", 100);
+
+        productionUnitManager.createMachine("Machine", 100);
+        productionUnitManager.createMachine("Machine", 100);
+        productionUnitManager.createMachine("Machine", 100);
 
         Factory factory = Factory.builder()
                 .setName("Test factory")
@@ -39,6 +45,12 @@ public class FactoryBuilderTest {
         factory.simulate();
 
         assertEquals("Test factory", factory.getName());
+        assertEquals(4, factory.getRepairmanPool().getRepairmanList().size());
+        assertEquals(2, factory.getProductionUnitManager().getAvailableUnits().get("W").size());
+        assertEquals(4, factory.getProductionUnitManager().getAvailableUnits().get("R").size());
+        assertEquals(3, factory.getProductionUnitManager().getAvailableUnits().get("M").size());
         assertEquals(pool, factory.getRepairmanPool());
+        // pool set to listen to OutageEvents
+        assertTrue(EventBusManager.getEventBus(Factory.EVENTBUS_NAME).getEventTypeListenersMap().get(EventType.OUTAGE).contains(pool));
     }
 }
