c512ba0e4046bae69d21212f54fbdc33a6d9a275
Maksym Zavada <m.zavada2005@ukr.net>
2025-01-15 01:43:24 +0100

    Hot fix

--- src/main/java/cz/cvut/fel/omo/smartfactory/equipment/Machine.java
+++ src/main/java/cz/cvut/fel/omo/smartfactory/equipment/Machine.java
@@ -2,12 +2,16 @@ package cz.cvut.fel.omo.smartfactory.equipment;
 
 import cz.cvut.fel.omo.smartfactory.CommonMaterial;
 import cz.cvut.fel.omo.smartfactory.identifier.Identifier;
+import cz.cvut.fel.omo.smartfactory.utils.JobUtils;
 import cz.cvut.fel.omo.smartfactory.visitor.FactoryVisitor;
 
 /**
  * The machine
  */
 public final class Machine extends AbstractEquipment {
+
+    private final float JOB_STEP = JobUtils.stepDuration(2);
+
     /**
      * Create factory equipment
      *
@@ -22,6 +26,8 @@ public final class Machine extends AbstractEquipment {
     public void process(long dt) {
         setActualHealth(-0.2f * dt + getActualHealth());
         usageTime += dt;
+
+        jobProgress += JOB_STEP;
 
         this.getUsageConsumer().accept(1.0f); // One usage per call this method
         this.getElectricityConsumer().accept(20.0f); // 20 kW/hour
